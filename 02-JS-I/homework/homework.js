9e8fb65cf0f7109945961625218caade2c975986
Carlosbaez22 <carlosbaez2235@gmail.com>
2022-02-06 20:35:23 -0300

    empty three

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -177,12 +177,13 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-  if (numero > 0 ){
-    return ("es positivo");
-  }
-  else{
-    return false;
-  }
+if (numero === 0){
+  return false;
+} else if (numero < 0){
+  return "Es negativo";
+} else {
+  return "Es positivo";
+}  
 
 }
      
@@ -252,6 +253,9 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
+  if (letra == a,e,i,o,u){
+    return "Es vocal";
+  }
    
   
 }
